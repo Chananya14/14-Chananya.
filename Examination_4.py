b9d4eaf3c3d6a3783b12d6459c371ebc782cb545
14-Chananya <chananya.daisy@gmail.com>
2026-09-17 15:36:26 +0700

    Add pricing logic based on product weight input

--- Examination_4.py
+++ Examination_4.py
@@ -7,4 +7,10 @@
 
 
 # นักเรียนเขียนโค้ดต่อจากบรรทัดนี้
-weight = float(input())
+weight = float(input("กรุณากรอกน้ำหนักสินค้า (kg.): "))
+if weight<=1:
+    print("จ่าย 30 บาท")
+elif weight<=5:
+    print("จ่าย 50 บาท")
+else:
+    print("จ่าย 100 บาท")
